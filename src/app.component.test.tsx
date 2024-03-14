357d9a6e4e8c241a82deeae95b5aba445b8aae66
Cam Dziurgot <cam.dziurgot@gartner.com>
2024-03-14 11:08:40 -0400

    tests

--- src/app.component.test.tsx
+++ src/app.component.test.tsx
@@ -1,9 +1,48 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import AppComponent from "./app.component";
+import {AppContextProvider} from "./app.context";
 
-test("renders learn react link", () => {
-  render(<AppComponent />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Component", () => {
+  it("context has description", async() => {
+    render(
+      <AppContextProvider>
+        <AppComponent />
+      </AppContextProvider>
+    );
+    expect(await screen.findByText("CONTEXT: Hello from App Component")).toBeInTheDocument();
+  });
+
+  it("signal has description", async() => {
+    render(
+      <AppContextProvider>
+        <AppComponent />
+      </AppContextProvider>
+    );
+    expect(await screen.findByText("SIGNAL: Hello from App Component")).toBeInTheDocument();
+  });
+
+  it("changing text field updates signal", async() => {
+    render(
+      <AppContextProvider>
+        <AppComponent />
+      </AppContextProvider>
+    );
+
+    expect(await screen.findByText("SIGNAL: Hello from App Component")).toBeInTheDocument();
+    fireEvent.change(screen.getByRole("textbox"), { target: { value: "Hello from Test" } });
+    expect(await screen.findByText("SIGNAL: Hello from Test")).toBeInTheDocument();
+  });
+
+  it("changing text field does NOT updates context", async() => {
+    render(
+      <AppContextProvider>
+        <AppComponent />
+      </AppContextProvider>
+    );
+
+    expect(await screen.findByText("SIGNAL: Hello from App Component")).toBeInTheDocument();
+    fireEvent.change(screen.getByRole("textbox"), { target: { value: "Hello from Test" } });
+    expect(await screen.findByText("CONTEXT: Hello from App Component")).toBeInTheDocument();
+  });
 });
